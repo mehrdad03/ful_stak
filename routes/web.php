b6f56fa014ec2bb59e266618e9092bdc7b4b3539
LeGion <mehrdad.dadashi@gmail.com>
2024-01-12 13:00:36 +0300

    created client auth and pricing component adn routs

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 
+/*********** Client *************/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', \App\Livewire\Client\Home\Index::class)->name('client.home');
+Route::get('/pricing', \App\Livewire\Client\Pricing\Index::class)->name('client.pricing');
+
+
+/*********** Client Login  & Register *************/
+
+Route::group(['prefix' => 'auth', 'middleware' => 'guest:web'], function () {
+
+    Route::name('auth.client')->group(function () {
+        Route::get('/client', \App\Livewire\Client\Auth\Index::class);
+
+    });
+
 });
 
+
+
+/*********** ADMIN PANEL *************/
 
 Route::group(['prefix' => 'admin', /*'middleware' => 'auth:admin'*/], function () {
 
