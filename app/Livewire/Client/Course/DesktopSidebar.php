cf0db7edbfe3bc629f4c3e419ef737556cf1fb1a
LeGion <mehrdad.dadashi@gmail.com>
2024-02-26 09:09:18 +0300

    create basket model and migration

--- app/Livewire/Client/Course/DesktopSidebar.php
+++ app/Livewire/Client/Course/DesktopSidebar.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 class DesktopSidebar extends Component
 {
     public $price;
+
+    public function addToBasket()
+    {
+
+
+    }
     public function render()
     {
         return view('livewire.client.course.desktop-sidebar');
