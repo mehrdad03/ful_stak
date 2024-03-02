bb6725173440f3236cc2a220409f47487e57ee2f
LeGion <mehrdad.dadashi@gmail.com>
2024-03-02 10:51:45 +0300

    design order process migrations

--- database/migrations/2024_03_02_070515_create_orders_table.php
+++ database/migrations/2024_03_02_070515_create_orders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->unsignedInteger('amount');
+            $table->unsignedInteger('discount')->default(0);
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(false);
+            $table->boolean('pay_status')->default(false);
             $table->timestamps();
         });
     }
