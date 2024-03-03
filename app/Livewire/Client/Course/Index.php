2ab66d42230e43d47c36abf376d42d24f34387c8
LeGion <mehrdad.dadashi@gmail.com>
2024-03-03 13:45:34 +0300

    fix bug in the client course index component

--- app/Livewire/Client/Course/Index.php
+++ app/Livewire/Client/Course/Index.php
@@ -12,8 +12,8 @@ class Index extends Component
 
     public function mount(Course $course)
     {
-        Session::forget('courseId');
-        $this->course = $course->with('sections.sectionLectures')->firstOrFail();
+
+        $this->course = $course->load('sections.sectionLectures');
 
     }
 
