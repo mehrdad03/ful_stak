17bcf1a39f33546b6f66ae266522fc0ef2199460
LeGion <mehrdad.dadashi@gmail.com>
2024-03-03 08:27:52 +0300

    refactor routs

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,42 @@
 <?php
 
+use App\Http\Controllers\uploadVideo as adminCourseVideoUpload;
+use App\Http\Livewire\Admin\Ck\Upload as adminVideoCkUpload;
+use App\Livewire\Client\Category\RoadMap;
+use App\Livewire\Client\Home\Index;
+use App\Livewire\Client\Pricing\Index as pricingIndex;
+use App\Livewire\Client\Course\Index as courseIndex;
+use App\Livewire\Client\Auth\Index as authIndex;
+use App\Livewire\Client\Basket\Index as basketIndex;
 use App\Livewire\Client\Profile\Comments;
 use App\Livewire\Client\Profile\Dashboard;
 use App\Livewire\Client\Profile\Financial;
 use App\Livewire\Client\Profile\Messages;
 use App\Livewire\Client\Profile\MyCourses;
 use App\Livewire\Client\Profile\Questions;
+use App\Livewire\Admin\Dashboard\Index as adminDashboardIndex;
+use App\Livewire\Admin\Course\Index as adminCourseIndex;
+use App\Livewire\Admin\Course\Section as adminCourseSection;
+use App\Livewire\Admin\Course\Lecture as adminCourseLecture;
+use App\Livewire\Admin\Course\Create as adminCourseCreate;
+use App\Livewire\Admin\Course\Seo as adminCourseSeo;
+use App\Livewire\Admin\User\Index as adminUserIndex;
 use Illuminate\Support\Facades\Route;
 
 
 /*********** Client  *************/
 
-Route::get('/', \App\Livewire\Client\Home\Index::class)->name('client.home');
-Route::get('/pricing', \App\Livewire\Client\Pricing\Index::class)->name('client.pricing');
-Route::get('/course/{course:url_slug}', \App\Livewire\Client\Course\Index::class)->name('client.course');
-Route::get('/auth/client/logout', [\App\Livewire\Client\Auth\Index::class, 'clientLogout'])->name('client.logout')->middleware('auth:web');
+Route::get('/', Index::class)->name('client.home');
+Route::get('/pricing', pricingIndex::class)->name('client.pricing');
+Route::get('/course/{course:url_slug}', courseIndex::class)->name('client.course');
+Route::get('/auth/client/logout', [authIndex::class, 'clientLogout'])->name('client.logout')->middleware('auth:web');
 
 
-Route::get('/road-map/{category:url_slug}', \App\Livewire\Client\Category\RoadMap::class)->name('client.category.road-map');
+Route::get('/road-map/{category:url_slug}', RoadMap::class)->name('client.category.road-map');
 
 
-
-
-Route::get('/cart', App\Livewire\Client\Basket\Index::class)->name('client.basket');
-Route::get('/payment/verify', [App\Livewire\Client\Basket\Index::class,'zarinPalPayment'])->name('client.zarinpal.verify');
-
-
-
+Route::get('/cart', basketIndex::class)->name('client.basket')->middleware('auth');
+Route::get('/payment/verify', [basketIndex::class, 'zarinPalPayment'])->name('client.zarinpal.verify')->middleware('auth');
 
 
 /*********** Client Login  & Register *************/
@@ -34,9 +44,9 @@ Route::get('/payment/verify', [App\Livewire\Client\Basket\Index::class,'zarinPal
 Route::group(['prefix' => 'auth', 'middleware' => 'guest:web'], function () {
 
     Route::name('auth.client')->group(function () {
-        Route::get('/client', \App\Livewire\Client\Auth\Index::class);
-        Route::get('/gmail', [\App\Livewire\Client\Auth\Index::class, 'redirectToProvider'])->name('.gmail');
-        Route::get('/gmail/callback', [\App\Livewire\Client\Auth\Index::class, 'handleProviderCallback'])->name('.gmail.callback');
+        Route::get('/client', authIndex::class);
+        Route::get('/gmail', [authIndex::class, 'redirectToProvider'])->name('.gmail');
+        Route::get('/gmail/callback', [authIndex::class, 'handleProviderCallback'])->name('.gmail.callback');
         //Route::get('/github', [\App\Livewire\Client\Auth\Index::class, 'redirectToGithubProvider'])->name('.github');
         //Route::get('/github/callback', [\App\Livewire\Client\Auth\Index::class, 'handleGithubProviderCallback'])->name('.github.callback');
 
@@ -46,7 +56,7 @@ Route::group(['prefix' => 'auth', 'middleware' => 'guest:web'], function () {
 
 /*********** Client Profile  *************/
 
-Route::group(['prefix' => 'profile', /*'middleware' => 'guest:web'*/], function () {
+Route::group(['prefix' => 'profile', 'middleware' => 'auth'], function () {
 
     Route::name('client.profile')->group(function () {
         Route::get('/dashboard', Dashboard::class)->name('dashboard');
@@ -63,14 +73,14 @@ Route::group(['prefix' => 'profile', /*'middleware' => 'guest:web'*/], function 
 Route::group(['prefix' => 'admin', /*'middleware' => 'auth:admin'*/], function () {
 
     Route::name('admin.')->group(function () {
-        Route::get('/dashboard', App\Livewire\Admin\Dashboard\Index::class)->name('dashboard');
-        Route::get('/courses', App\Livewire\Admin\Course\Index::class)->name('course.index');
-        Route::get('/course/{courseId}/section', App\Livewire\Admin\Course\Section::class)->name('course.section');
-        Route::get('/course/section/{sectionId}/lecture', App\Livewire\Admin\Course\Lecture::class)->name('course.section.lecture');
-        Route::get('/course/create', App\Livewire\Admin\Course\Create::class)->name('course.create');
-        Route::get('/course/{courseId}/seo', App\Livewire\Admin\Course\Seo::class)->name('course.seo');
-        Route::post('/ck-upload', [\App\Http\Livewire\Admin\Ck\Upload::class, 'upload'])->name('ck-upload');
-        Route::get('/users', \App\Livewire\Admin\User\Index::class)->name('users');
-        Route::post('/upload-video/{courseId?}/{lectureId?}/{sectionId?}', [\App\Http\Controllers\uploadVideo::class, 'uploadVideo'])->name('upload-video');
+        Route::get('/dashboard', adminDashboardIndex::class)->name('dashboard');
+        Route::get('/courses', adminCourseIndex::class)->name('course.index');
+        Route::get('/course/{courseId}/section', adminCourseSection::class)->name('course.section');
+        Route::get('/course/section/{sectionId}/lecture', adminCourseLecture::class)->name('course.section.lecture');
+        Route::get('/course/create', adminCourseCreate::class)->name('course.create');
+        Route::get('/course/{courseId}/seo', adminCourseSeo::class)->name('course.seo');
+        Route::post('/ck-upload', [adminVideoCkUpload::class, 'upload'])->name('ck-upload');
+        Route::get('/users', adminUserIndex::class)->name('users');
+        Route::post('/upload-video/{courseId?}/{lectureId?}/{sectionId?}', [adminCourseVideoUpload::class, 'uploadVideo'])->name('upload-video');
     });
 });
