8288493bfa094704295bd04f95106e579b41ec4c
LeGion <mehrdad.dadashi@gmail.com>
2024-03-15 11:01:09 +0300

    get transactions history in the Financial.php component and refactor table structure

--- app/Livewire/Client/Profile/Financial.php
+++ app/Livewire/Client/Profile/Financial.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Client\Profile;
 
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Financial extends Component
 {
+    public $transactions = [];
+
+    public function mount()
+    {
+        $this->transactions = Transaction::query()->where('user_id', Auth::id())
+            ->latest()->get();
+
+    }
+
     public function render()
     {
         return view('livewire.client.profile.financial');
