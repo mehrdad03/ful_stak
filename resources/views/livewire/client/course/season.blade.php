f7121c489e42f437408481affa195307e1057692
LeGion <mehrdad.dadashi@gmail.com>
2024-08-03 22:35:27 +0300

    refactor

--- resources/views/livewire/client/course/season.blade.php
+++ resources/views/livewire/client/course/season.blade.php
@@ -106,16 +106,7 @@
                             <div class="d-flex column-gap-2 align-items-center justify-content-between lecture-info w-25" wire:ignore>
 
                                 <p class="m-0 text-primary ">{{@date('s : i',$lecture->duration)}}</p>
-                                @if($checkPurchase or \Illuminate\Support\Facades\Auth::id()==1)
-                                    <button data-path="{{config('app.ftp_url').@$lecture->video->path }}"
-                                            data-title="{{$lecture->title }}" data-bs-toggle="modal"
-                                            data-bs-target="#videoModal"
-                                            data-toggle="modal"
-                                            class="text-white bg-secondary main-btn videoModal  rounded-2 px-2 py-1"  wire:click="getLectureId({{$lecture->id}})"
-                                            type="button">
-                                        مشاهده
-                                    </button>
-                                @elseif($lecture->free)
+
 
                                     <button data-path="{{config('app.ftp_url').@$lecture->video->path }}"
                                             data-title="{{$lecture->title }}" data-bs-toggle="modal"
@@ -125,16 +116,6 @@
                                             type="button">
                                         مشاهده
                                     </button>
-                                @else
-
-                                    <div style="background: rgb(232 28 77)" class="px-1 rounded-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: #fff"
-                                             viewBox="0 0 24 24">
-                                            <path
-                                                d="M17 9.761v-4.761c0-2.761-2.238-5-5-5-2.763 0-5 2.239-5 5v4.761c-1.827 1.466-3 3.714-3 6.239 0 4.418 3.582 8 8 8s8-3.582 8-8c0-2.525-1.173-4.773-3-6.239zm-8-4.761c0-1.654 1.346-3 3-3s3 1.346 3 3v3.587c-.927-.376-1.938-.587-3-.587s-2.073.211-3 .587v-3.587zm3 17c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6zm2-6c0 1.104-.896 2-2 2s-2-.896-2-2 .896-2 2-2 2 .896 2 2z"/>
-                                        </svg>
-                                    </div>
-                                @endif
                             </div>
                         </div>
                     @endforeach
